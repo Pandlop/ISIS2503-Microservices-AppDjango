1383074addcab3edb909370964cc6c19f1d2695d
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-18 13:25:10 -0500

    a

--- places/places/urls.py
+++ places/places/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 urlpatterns = [
-    path('places/', views.PlaceList),
+    url(r'^places/', views.PlaceList),
     path('placecreate/', csrf_exempt(views.PlaceCreate), name='placeCreate'),
     path('createplaces/', csrf_exempt(views.PlacesCreate), name='createPlaces'),
 ]
